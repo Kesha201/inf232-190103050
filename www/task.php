11f86f76036d90db94ae2255e2a39e094acd9db0
Yerkem <erkegalymbekova@gmail.com>
2021-02-08 12:09:21 +0600

    php file for new tasks

---
+++ www/task.php
@@ -0,0 +1,9 @@
+<?php
+
+   require_once('./php/databasefunctions.php');
+
+$result = ConnectCelebDataBase(0); /* located in /php/databasefunctions.php */
+
+/* echo '<br>the result is *'.$result.'*'; */
+
+?>
